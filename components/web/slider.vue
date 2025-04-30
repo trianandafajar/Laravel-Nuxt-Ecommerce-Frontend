3970586798a6f1bf3351d46aa8bbbffd74721681
mail.trianandafajar <mail.trianandafajar@gmail.com>
2025-04-30 15:58:16 +0700

    Update Slider

--- components/web/slider.vue
+++ components/web/slider.vue
@@ -1,50 +1,43 @@
 <template>
   <div class="container-fluid mt-custom">
-    <div id="mycarousel" class="carousel slide" data-ride="carousel" data-interval="4000">
-      
+    <div id="mycarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="4000">
       <div class="carousel-inner">
-
-        <div class="carousel-item" v-for="(slider, id) in sliders" :class="{ active: id==0 }" :key='slider.id'>
-            <a :href="slider.link" target="_blank">
-                <img :src="slider.image" class="d-block w-100 rounded">
-            </a> 
+        <!-- Iterating over sliders array -->
+        <div class="carousel-item" v-for="(slider, id) in sliders" :class="{ active: id === 0 }" :key="slider.id">
+          <a :href="slider.link" target="_blank">
+            <img :src="slider.image" class="d-block w-100 rounded" :alt="'Slider ' + id" />
+          </a>
         </div>
-
-      </div> 
-
-      <a class="carousel-control-prev" href="#mycarousel" role="button" data-slide="prev">
-        <div class="banner-icons"> <span class="fa fa-angle-left"></span> </div> <span class="sr-only">Previous</span>
-      </a> 
-      <a class="carousel-control-next" href="#mycarousel" role="button" data-slide="next">
-        <div class="banner-icons"> <span class="fa fa-angle-right"></span> </div> <span class="sr-only">Next</span>
-      </a>
+      </div>
+      
+      <!-- Carousel Controls -->
+      <button class="carousel-control-prev" type="button" data-bs-target="#mycarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#mycarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-
-    //hook "fetch"
-    async fetch() {
-
-      //fething sliders on Rest API
-      await this.$store.dispatch('web/slider/getSlidersData')
-
-    },
-
-    //computed
-    computed: {
-
-      //sliders
-      sliders() {
-        return this.$store.state.web.slider.sliders
-      }
+export default {
+  async fetch() {
+    // Fetching sliders data from the API
+    await this.$store.dispatch('web/slider/getSlidersData');
+  },
+  computed: {
+    // Accessing sliders data from Vuex store
+    sliders() {
+      return this.$store.state.web.slider.sliders;
     }
-
   }
+}
 </script>
 
-<style>
-
+<style scoped>
+/* Optional: Add custom styles for the carousel if needed */
 </style>
